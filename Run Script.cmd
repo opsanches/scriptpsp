407f157210480b95011656e3e542c8425a106c09
Pedro Sanches <124384340+opsanches@users.noreply.github.com>
2023-03-09 12:49:21 -0300

    Typing Error

--- Run Script.cmd
+++ Run Script.cmd
@@ -1,4 +1,4 @@
 @echo off
 chcp 65001 >nul
 
-powershell.exe -ExecutionPolicy Bypass -NoExit -NoProfile -NoLogo -File "%~dp0scrpt.ps1"
+powershell.exe -ExecutionPolicy Bypass -NoExit -NoProfile -NoLogo -File "%~dp0script.ps1"
